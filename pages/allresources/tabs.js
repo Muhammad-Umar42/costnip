4059f342fe27882094901e75f07d4878be8e8a56
mumar <umrking8877@gmail.com>
2023-04-19 02:48:25 +0500

    50%Done

--- pages/allresources/tabs.js
+++ pages/allresources/tabs.js
@@ -6,20 +6,95 @@ const Tabs = () => {
 
     return (
         <>
-            <Tab.Group>
-                <div className="grid grid-cols-3 countain">
-                    <Tab.List>
-                        <Tab className='block'>Tab 1</Tab>
-                        <Tab className='block'>Tab 2</Tab>
-                        <Tab className='block'>Tab 3</Tab>
-                    </Tab.List>
-                    <Tab.Panels className='col-span-2'>
-                        <Tab.Panel>Content 1</Tab.Panel>
-                        <Tab.Panel>Content 2</Tab.Panel>
-                        <Tab.Panel>Content 3</Tab.Panel>
-                    </Tab.Panels>
+            <div className='bg-gray-200 py-12'>
+                <div className="countain">
+                    <h1 className='font-bold text-2xl pb-4'>All Resources and Tools</h1>
+                    <Tab.Group>
+                        <div className="grid grid-cols-3 ">
+                            <Tab.List>
+                                <Tab className='block outline-0 mb-2'>Tab 1</Tab>
+                                <Tab className='block outline-0 mb-2'>Tab 2</Tab>
+                                <Tab className='block outline-0 mb-2'>Tab 3</Tab>
+                            </Tab.List>
+                            <Tab.Panels className='col-span-2'>
+                                <Tab.Panel className=''>
+                                    <div className='bg-white rounded-lg p-4 flex mb-3'>
+                                        <div className='m-[auto]'>
+                                            <img src="tab.png" alt="" />
+                                        </div>
+                                        <div className='px-4'>
+                                            <h1 className='font-bold'>Help Paying for insulin</h1>
+                                            <div className="divider"></div>
+                                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsum voluptatibus sapiente sit perspiciatis ratione? Doloremque rerum rem dolore voluptatem minus delectus cum tempora? Harum unde quia architecto distinctio ipsa?</h1>
+                                            <h6 className='font-bold text-blue-600'>Learn more</h6>
+                                        </div>
+                                    </div>
+                                </Tab.Panel>
+                                <Tab.Panel>
+                                    <div className=' bg-white rounded-lg p-4 flex mb-3'>
+                                        <div className='m-[auto]'>
+                                            <img src="tab2.png" alt="" />
+                                        </div>
+                                        <div className='px-4'>
+                                            <h1 className='font-bold'>Help Paying for insulin</h1>
+                                            <div className="divider"></div>
+                                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsum voluptatibus sapiente sit perspiciatis ratione? Doloremque rerum rem dolore voluptatem minus delectus cum tempora? Harum unde quia architecto distinctio ipsa?</h1>
+                                            <h6 className='font-bold text-blue-600'>Learn more</h6>
+                                        </div>
+                                    </div>
+                                    <div className='bg-white rounded-lg p-4 flex mb-3 '>
+                                        <div className='m-[auto]'>
+                                            <img src="tab2.png" alt="" />
+                                        </div>
+                                        <div className='px-4'>
+                                            <h1 className='font-bold'>Help Paying for insulin</h1>
+                                            <div className="divider"></div>
+                                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsum voluptatibus sapiente sit perspiciatis ratione? Doloremque rerum rem dolore voluptatem minus delectus cum tempora? Harum unde quia architecto distinctio ipsa?</h1>
+                                            <h6 className='font-bold text-blue-600'>Learn more</h6>
+                                        </div>
+                                    </div>
+                                </Tab.Panel>
+                                <Tab.Panel>
+                                    <div className='bg-white rounded-lg p-4 flex mb-3'>
+                                        <div className='m-[auto]'>
+                                            <img src="tab3.jpg" alt="" />
+                                        </div>
+                                        <div className='px-4'>
+                                            <h1 className='font-bold'>Help Paying for insulin</h1>
+                                            <div className="divider"></div>
+                                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsum voluptatibus sapiente sit perspiciatis ratione? Doloremque rerum rem dolore voluptatem minus delectus cum tempora? Harum unde quia architecto distinctio ipsa?</h1>
+                                            <h6 className='font-bold text-blue-600'>Learn more</h6>
+                                        </div>
+                                    </div>
+                                    <div className='bg-white rounded-lg p-4 flex mb-3'>
+                                        <div className='m-[auto]'>
+                                            <img src="tab3.jpg" alt="" />
+                                        </div>
+                                        <div className='px-4'>
+                                            <h1 className='font-bold'>Help Paying for insulin</h1>
+                                            <div className="divider"></div>
+                                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsum voluptatibus sapiente sit perspiciatis ratione? Doloremque rerum rem dolore voluptatem minus delectus cum tempora? Harum unde quia architecto distinctio ipsa?</h1>
+                                            <h6 className='font-bold text-blue-600'>Learn more</h6>
+                                        </div>
+                                    </div>
+                                    <div className='bg-white rounded-lg p-4 flex mb-3'>
+                                        <div className='m-[auto]'>
+                                            <img src="tab3.jpg" alt="" />
+                                        </div>
+                                        <div className='px-4'>
+                                            <h1 className='font-bold'>Help Paying for insulin</h1>
+                                            <div className="divider"></div>
+                                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsum voluptatibus sapiente sit perspiciatis ratione? Doloremque rerum rem dolore voluptatem minus delectus cum tempora? Harum unde quia architecto distinctio ipsa?</h1>
+                                            <h6 className='font-bold text-blue-600'>Learn more</h6>
+                                        </div>
+                                    </div>
+                                </Tab.Panel>
+
+                            </Tab.Panels>
+                        </div>
+                    </Tab.Group>
                 </div>
-            </Tab.Group>
+            </div>
         </>
     )
 }
